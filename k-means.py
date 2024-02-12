b5564267f1c6934e4b9772a1e1c06dfb98e284a3
Yovany samaca <egsamaca56@gmail.com>
2024-02-11 19:31:13 -0500

    Con k-means segmento los estudiantes y genero un nuevo dataset con donde 0 es nivel bajo 1 es nivel medio y 2 es nivel alto

--- k-means.py
+++ k-means.py
@@ -9,5 +9,11 @@ my_model = 'K-means'
 data = pd.read_csv('data_model.csv')
 X = data
 
-print(X.dtypes)
+kmeans = MiniBatchKMeans(n_clusters=3, batch_size=8).fit(X)
+print(f" Ejecutano -> {my_model}")
+data['GRUPO'] = kmeans.predict(X)
+print('Graficando el resultado')
+sns.histplot(data=data, x='PUNT_GLOBAL', hue='GRUPO', multiple='stack')
+plt.show()
+data.to_csv('data_model_segmented.csv', index = False)
 
