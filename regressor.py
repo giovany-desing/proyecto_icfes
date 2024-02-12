a9f0b7bed7dfde4e4f5aaa9d18002c99d669b8fc
Yovany samaca <egsamaca56@gmail.com>
2024-02-11 20:21:37 -0500

    regresion lineal simple para predecir el puntaje final de los estudiantes

--- regressor.py
+++ regressor.py
@@ -13,7 +13,18 @@ puntajes = data[['MOD_RAZONA_CUANTITATIVO_PNAL',
        'MOD_LECTURA_CRITICA_PNAL', 'MOD_COMPETEN_CIUDADA_PNAL',
        'MOD_INGLES_PNAL', 'MOD_COMUNI_ESCRITA_PNAL', 'PUNT_GLOBAL']]
 
-
+X = puntajes.drop(['PUNT_GLOBAL'],axis=1)
+y = puntajes['PUNT_GLOBAL']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25,random_state=0)
+#iniciando a predecir con el modelo
+model = LinearRegression().fit(X_train,y_train)
+y_predict = model.predict(X_test)
+print('calculando los coheficientes')
+print()
+print(f'coheficientes {model.coef_}')
+print()
+print('Calculando el score')
+print(model.score(X_test,y_test))
 
 
 
